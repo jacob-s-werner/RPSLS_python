0bc275993909d2f85076172cb6239400e6c5bf01
Jacob Werner <jacob.scott.werner@gmail.com>
2021-04-16 14:03:41 -0500

    Creates choose_gesture Function in Player Class; runs through the list of gestures available and has the user input a corresponding number to match the index, assigning the gesture to player.current_gesture.

--- player.py
+++ player.py
@@ -2,4 +2,22 @@ class Player:
     def __init__(self):
         self.name = "Player 1"
         self.score = 0
-        self.current_gesture
+        self.current_gesture = None
+        self.list_of_gestures = []
+
+    def choose_gesture(self):
+        keep_running = True
+
+        while keep_running:
+            print(f"{self.name} - enter the number to play from the following gestures")
+            i = 1
+            for gesture in self.list_of_gestures:
+                print(f"{i}: {gesture.name}")
+                i += 1
+
+            try:
+                chosen_gesture = int(input("Enter Number: "))
+                self.current_gesture = self.list_of_gestures[chosen_gesture]
+                break
+            except ValueError:
+                print("Invalid Input " + ValueError)
